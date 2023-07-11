cc664d1f5b43e58c3e8a37c45e0dc7ce05e2d80f
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-07-11 15:59:39 +0100

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,17 @@ from copy import deepcopy
 class BaseModel():
     """ Base model class """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a BaseModel instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    self.__setattr__(key,  datetime(value))
+                    continue
+                self.__setattr__(key, value)
+            return
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
