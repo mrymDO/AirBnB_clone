487630348b8c2653283c8c444ebf5ffba5b37636
Meryem <dobli.meryeme@gmail.com>
2023-07-11 03:47:28 -0700

    fix docstring and pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!usr/bin/python3
-""" Base Class Module """
+
+"""Base Class Module"""
 
 import uuid
 from datetime import datetime
@@ -8,16 +9,22 @@ from copy import deepcopy
 
 class BaseModel():
     """ Base model class """
-    
+
     def __init__(self):
+        """Initialize a BaseModel instance"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+
     def save(self):
+        """Update the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Return a dictionary representation of the instance
+        containing all attributes
+        """
         new_dict = deepcopy(self.__dict__)
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
@@ -25,4 +32,11 @@ class BaseModel():
         return new_dict
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """
+        Return a string representation of the instance
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__
+        )
