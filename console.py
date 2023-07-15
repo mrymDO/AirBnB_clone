5c5aa6d63df49c9081f0d056b720b6a335d0ac19
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-07-15 15:54:30 +0100

    task 7

--- console.py
+++ console.py
@@ -37,80 +37,80 @@ class HBNBCommand(cmd.Cmd):
         """Override emptyline method to do nothing"""
         pass
 
-    def do_create(self, arg):
-        """Creates a new instance of BaseModel"""
-        arg_list = shlex.split(arg)
-        if len(arg_list) == 0:
-            print("** class name missing **")
-        elif arg_list[0] not in self.class_mapping:
-            print("** class doesn't exist **")
-        else:
-            obj = self.class_mapping[arg_list[0]]()
-            print(obj.id)
-            obj.save()
+    # def do_create(self, arg):
+    #     """Creates a new instance of BaseModel"""
+    #     arg_list = shlex.split(arg)
+    #     if len(arg_list) == 0:
+    #         print("** class name missing **")
+    #     elif arg_list[0] not in self.class_mapping:
+    #         print("** class doesn't exist **")
+    #     else:
+    #         obj = self.class_mapping[arg_list[0]]()
+    #         print(obj.id)
+    #         obj.save()
 
-    def do_show(self, arg):
-        """Prints string representation of an instance"""
-        args = shlex.split(arg)
-        if len(args) == 0:
-            print("** class name missing **")
-        elif len(args) == 1:
-            if args[0] in self.class_mapping:
-                print("** instance id missing **")
-            else:
-                print("** class doesn't exist **")
-        else:
-            class_name = args[0]
-            obj_id = args[1]
-            key = f"{class_name}.{obj_id}"
-            all_objs = models.storage.all()
+    # def do_show(self, arg):
+    #     """Prints string representation of an instance"""
+    #     args = shlex.split(arg)
+    #     if len(args) == 0:
+    #         print("** class name missing **")
+    #     elif len(args) == 1:
+    #         if args[0] in self.class_mapping:
+    #             print("** instance id missing **")
+    #         else:
+    #             print("** class doesn't exist **")
+    #     else:
+    #         class_name = args[0]
+    #         obj_id = args[1]
+    #         key = f"{class_name}.{obj_id}"
+    #         all_objs = models.storage.all()
 
-            if class_name not in self.class_mapping:
-                print("** class doesn't exist **")
-            elif key not in all_objs:
-                print("** no instance found **")
-            else:
-                print(all_objs[key])
+    #         if class_name not in self.class_mapping:
+    #             print("** class doesn't exist **")
+    #         elif key not in all_objs:
+    #             print("** no instance found **")
+    #         else:
+    #             print(all_objs[key])
 
-    def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id"""
-        args = arg.split()
-        if len(args) == 0:
-            print("** class name missing **")
-        elif len(args) == 1:
-            if args[0] in self.class_mapping:
-                print("** instance id missing **")
-            else:
-                print("** class doesn't exist **")
-        else:
-            class_name = args[0]
-            obj_id = args[1]
-            all_objs = models.storage.all()
-            key = f"{class_name}.{obj_id}"
-            if key in all_objs:
-                del all_objs[key]
-                models.storage.save()
-            else:
-                print("** no instance found **")
+    # def do_destroy(self, arg):
+    #     """Deletes an instance based on the class name and id"""
+    #     args = arg.split()
+    #     if len(args) == 0:
+    #         print("** class name missing **")
+    #     elif len(args) == 1:
+    #         if args[0] in self.class_mapping:
+    #             print("** instance id missing **")
+    #         else:
+    #             print("** class doesn't exist **")
+    #     else:
+    #         class_name = args[0]
+    #         obj_id = args[1]
+    #         all_objs = models.storage.all()
+    #         key = f"{class_name}.{obj_id}"
+    #         if key in all_objs:
+    #             del all_objs[key]
+    #             models.storage.save()
+    #         else:
+    #             print("** no instance found **")
 
-    def do_all(self, args):
-        """ Show All instance """
-        arg = shlex.split(args)
-        if arg and arg[0] not in self.class_mapping:
-            print("** class doesn't exist **")
-            return
+    # def do_all(self, args):
+    #     """ Show All instance """
+    #     arg = shlex.split(args)
+    #     if arg and arg[0] not in self.class_mapping:
+    #         print("** class doesn't exist **")
+    #         return
 
-        list_objs = []
-        all_objs = models.storage.all().items()
-        if len(arg) > 0:
-            for key, value in all_objs:
-                if value["__class__"] in self.class_mapping:
-                    list_objs.append(str(value))
-        else:
-            for key, value in all_objs:
-                list_objs.append(str(value))
-        if len(list_objs) > 0:
-            print(list_objs)
+    #     list_objs = []
+    #     all_objs = models.storage.all().items()
+    #     if len(arg) > 0:
+    #         for key, value in all_objs:
+    #             if value["__class__"] in self.class_mapping:
+    #                 list_objs.append(str(value))
+    #     else:
+    #         for key, value in all_objs:
+    #             list_objs.append(str(value))
+    #     if len(list_objs) > 0:
+    #         print(list_objs)
 
     def do_update(self, args):
         """update"""
