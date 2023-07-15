35fb3df80865893219cc697fae6c9f632e2315d3
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-07-15 18:08:01 +0100

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,6 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        if self not in models.storage.all():
             models.storage.new(self)
 
     def save(self):
