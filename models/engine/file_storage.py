8e8765afe48d6fc02208c79fed9e50fae830bb9c
Meryem <dobli.meryeme@gmail.com>
2023-07-15 04:03:12 -0700

    private class attribute

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,12 +30,12 @@ class FileStorage:
 
     def all(self):
         """return the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
@@ -43,19 +43,19 @@ class FileStorage:
         for key, obj in self.__objects.items():
             json_dict[key] = obj.to_dict()
         json_string = json.dumps(json_dict)
-        with open(self.__file_path, "w") as f:
+        with open(FileStorage.__file_path, "w") as f:
             f.write(json_string)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 data = json.load(f)
                 for key, obj_dict in data.items():
                     class_name, obj_id = key.split('.')
                     class_ = self.class_mapping.get(class_name)
                     if class_ is not None:
                         obj = class_(**obj_dict)
-                        self.__objects[key] = obj
+                        FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
