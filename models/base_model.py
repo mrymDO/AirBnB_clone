731a3134393b6af8fb27192b1a9ecc4148c29484
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-07-15 18:07:14 +0100

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -26,14 +26,14 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+        if self not in models.storage.all():
             models.storage.new(self)
 
     def save(self):
         """Update the updated_at attribute with the current datetime"""
 
         self.updated_at = datetime.now()
-        
+
         models.storage.save()
 
     def to_dict(self):
