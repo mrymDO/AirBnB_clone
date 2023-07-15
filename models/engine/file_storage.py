73bd0da841b40c5eee09facebf13ac3d49665120
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-07-15 17:52:34 +0100

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,7 +35,7 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        if obj:
+        if obj and isinstance(obj, BaseModel):
             key = obj.__class__.__name__ + '.' + obj.id
             FileStorage.__objects[key] = obj
 
